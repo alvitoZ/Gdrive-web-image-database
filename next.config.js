d8a5804beb2ee08b4d04a42b581b0cebcc479ee0
alvitoZ <107417445+alvitoZ@users.noreply.github.com>
2023-09-28 01:03:59 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,18 +6,15 @@ const nextConfig = {
     DATABASE: process.env.DATABASE,
   },
 {
-  headers: [
-    {
-      source: "/api/(.*)",
-      headers: [
-        { key: "Access-Control-Allow-Credentials", "value": "true" },
-        { key: "Access-Control-Allow-Origin", "value": "*" },
-        { key: "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-        { key: "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
-      ]
-    }
-  ]
-}
+  async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: '*',
+          },
+        ]
+      },
+},
   images: {
     domains: ["drive.google.com"],
     remotePatterns: [
