e6e85e396b2ca52dfe392784f996ade35f5e8f98
alvitoZ <107417445+alvitoZ@users.noreply.github.com>
2023-09-28 01:00:05 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,14 +5,19 @@ const nextConfig = {
     BASEURL: process.env.BASEURL,
     DATABASE: process.env.DATABASE,
   },
- async rewrites() {
-        return [
-          {
-            source: '/api/:path*',
-            destination: 'https://gdrive-web-image-database.vercel.app/api*',
-          },
-        ]
-      },
+{
+  headers: [
+    {
+      source: "/api/(.*)",
+      headers: [
+        { key: "Access-Control-Allow-Credentials", "value": "true" },
+        { key: "Access-Control-Allow-Origin", "value": "*" },
+        { key: "Access-Control-Allow-Methods", "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+        { key: "Access-Control-Allow-Headers", "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+      ]
+    }
+  ]
+}
   images: {
     domains: ["drive.google.com"],
     remotePatterns: [
